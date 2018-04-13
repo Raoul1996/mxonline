af1f737383df4283b855c2641322ef7ffd0409fd
Raoul1996 <baobaozhou199606@gmail.com>
2018-04-13 23:59:53 +0800

    add tpl

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -2,8 +2,19 @@
 __date__ = '2018/4/10 下午10:40'
 
 import xadmin
-
+from xadmin import views
 from .models import EmailVerifyRecord, Banner
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSetting(object):
+    site_title = 'mxOnline'
+    site_footer = 'mxOnline'
+    menu_style = 'accordion'
 
 
 class EmailVerifyRecordAdmin(object):
@@ -13,10 +24,12 @@ class EmailVerifyRecordAdmin(object):
 
 
 class BannerAdmin(object):
-    list_display = ['title', 'image', 'url', 'index','add_time']
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
     search_fields = ['title', 'image', 'url', 'index']
-    list_filter = ['title', 'image', 'url', 'index','add_time']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
 
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSetting)
